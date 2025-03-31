86ca72d2923ef47fbfff67b076a398399a3bfdbc
gippo05 <gsarinas17@gmail.com>
2025-04-01 01:33:27 +0800

    sad

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 const outputEl = document.getElementById('output-el')
+let result;
+let number1 = "";
+let number2= "";
+const num1 = document.getElementById('num1');
+
+num1.addEventListener("click", function(){
+    
+})
